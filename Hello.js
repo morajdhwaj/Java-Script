d591531672b8fcd72d5e732184b9643687a15ffa
moraj <dhwajmoraj7@gmail.com>
2023-04-02 11:53:04 +0530

    Arrow Function sum

--- Hello.js
+++ Hello.js
@@ -1,7 +1,9 @@
-const moraj = (x = 5, y = 5) => {
-  return x * y;
-};
+const x = sumOf(2, 3, 4, 6, 4, 4, 4, 5, 6, 6, 6);
 
-let x = moraj();
+function sumOf(...args) {
+  let sum = 0;
+  for (let arg of args) sum += arg;
+  return sum;
+}
 
 console.log(x);
