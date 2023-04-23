13218f0644996c1f3e01efaf18843862f3cb46d4
moraj <dhwajmoraj7@gmail.com>
2023-04-23 15:11:26 +0530

    Learn object

--- Hello.js
+++ Hello.js
@@ -1,9 +1,10 @@
-const x = sumOf(2, 3, 4, 6, 4, 4, 4, 5, 6, 6, 6);
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  language: "EN",
+  get fullName() {
+    return this.firstName + " " + this.lastName;
+  },
+};
 
-function sumOf(...args) {
-  let sum = 0;
-  for (let arg of args) sum += arg;
-  return sum;
-}
-
-console.log(x);
+console.log(person.fullName);
--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
 
-function App() {
-  const [inputs, setInputs] = useState({});
-
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(inputs);
-  };
-
+const App = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Enter your name:
-        <input
-          type="text"
-          name="username"
-          value={inputs.username || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Enter your age:
-        <input
-          type="number"
-          name="age"
-          value={inputs.age || ""}
-          onChange={handleChange}
-        />
-      </label>
-      <input type="submit" />
-    </form>
+    <div>
+      <h1>Hello World</h1>
+    </div>
   );
-}
+};
 
 export default App;
